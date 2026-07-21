4a04fdfc484ac5f64397d85e7ea10f93195790a0
Luis <84397555+LuisFadini@users.noreply.github.com>
2026-07-21 13:37:13 -0300

    test: modernize jest configuration and test file naming

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
-module.exports = {
+import { defineConfig } from 'jest'
+
+export default defineConfig({
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { babelConfig: false }],
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      { babelConfig: false, tsconfig: 'tsconfig.test.json' },
+    ],
   },
-  testRegex: '(/__tests__/.*|\\.(test))\\.(ts|js)$',
+
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+
   roots: ['<rootDir>/__tests__'],
-  testPathIgnorePatterns: ['/node_modules/', '\\.d\\.ts$'],
-}
+})
